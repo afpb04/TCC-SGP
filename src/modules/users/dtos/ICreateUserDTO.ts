29d5b4bf089c698bd07e954b7293ef334d637756
Antonio Francisco <afpb04@hotmail.com>
2020-12-06 17:12:46 -0300

    *-*

---
+++ src/modules/users/dtos/ICreateUserDTO.ts
@@ -0,0 +1,8 @@
+/* eslint-disable camelcase */
+export default interface ICreateUsersDTO {
+  name: string;
+  email: string;
+  password: string;
+  admin: boolean;
+  company_id: string;
+}
--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -1,44 +1,46 @@
 /* eslint-disable camelcase */
-import { getRepository } from 'typeorm';
 import { hash } from 'bcryptjs';
+import { injectable, inject } from 'tsyringe';
 import User from '@modules/users/infra/typeorm/entities/User';
-
 import AppError from '@shared/errors/AppError';
 
-interface Request {
+import IUsersRepository from '../repositories/IUsersRepository';
+
+interface IRequest {
   name: string;
   email: string;
   password: string;
   admin: boolean;
   company_id: string;
 }
+@injectable()
 class CreateUserService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+  ) {}
+
   public async execute({
     name,
     email,
     password,
     admin = true,
     company_id,
-  }: Request): Promise<User> {
-    const userRepository = getRepository(User);
-
-    const checkUserExists = await userRepository.findOne({
-      where: { email },
-    });
+  }: IRequest): Promise<User> {
+    const checkUserExists = await this.usersRepository.findByEmail(email);
     if (checkUserExists) {
       throw new AppError('Email address already used.');
     }
 
     const hashedPassword = await hash(password, 8);
 
-    const user = userRepository.create({
+    const user = await this.usersRepository.create({
       name,
       email,
       password: hashedPassword,
       admin,
       company_id,
     });
-    await userRepository.save(user);
 
     return user;
   }
