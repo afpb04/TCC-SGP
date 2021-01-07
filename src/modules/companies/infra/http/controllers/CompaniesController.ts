1e774282c9791c277b4f184f610d03afe8a433df
Antonio Francisco <afpb04@hotmail.com>
2021-01-07 13:06:38 -0300

    fix bugs

--- src/modules/companies/infra/http/controllers/CompaniesController.ts
+++ src/modules/companies/infra/http/controllers/CompaniesController.ts
@@ -1,15 +1,16 @@
+/* eslint-disable camelcase */
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
 import CreateCompanyService from '@modules/companies/services/CreateCompanyService';
 
 export default class CompaniesController {
   public async create(request: Request, response: Response): Promise<Response> {
-    const { name, cnpj } = request.body;
+    const { company_name, cnpj } = request.body;
 
     const createCompany = container.resolve(CreateCompanyService);
 
     const company = await createCompany.execute({
-      name,
+      name: company_name,
       cnpj,
     });
     return response.json(company);
